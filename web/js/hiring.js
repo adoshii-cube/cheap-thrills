ffd726be05928bbfc06d2e48a3bf8ce29e935643
adoshi <adoshi@192.168.1.10>
2017-03-03 15:28:25 +0530

    Recommendation table for hiring created with - 1) popup 2) table cell alignments for string and number 3) conditional formatting of cells, based on values

--- web/js/hiring.js
+++ web/js/hiring.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     plotHiringQ4Charts();
     $('body').find('a[href$="tab4-panel"]').removeClass('vertical-mdl-tabs-disabled');
 
-//    plotHiringQ5Charts();
+    plotHiringQ5Charts();
     $('body').find('a[href$="tab5-panel"]').removeClass('vertical-mdl-tabs-disabled');
 
     $(".mdl-tabs__tab").on("click", function () {
@@ -278,7 +278,7 @@ function plotHiringQ1Charts() {
         });
         chart4.render();
         chart5
-                .margins({top: 0, bottom: 30, left: 50, right: 20})
+                .margins({top: 20, bottom: 30, left: 50, right: 20})
                 .dimension(metricName5)
                 .group(metricNameGroup5)
                 .elasticY(true)
@@ -1385,4 +1385,240 @@ function plotHiringQ5Charts() {
     var chart2 = dc.selectMenu("#hiring_q5_chart2");
     var chart3 = dc.selectMenu("#hiring_q5_chart3");
     var chart4 = dc.barChart("#hiring_q5_chart4");
+    var chart5 = dc.dataTable("#hiring_q5_chart5");
+
+    d3.csv("hiring_q5.csv", function (error, data) {
+        var cf = crossfilter(data);
+
+        data.forEach(function (d) {
+            d.m4 = d3.time.format.utc("%d-%m-%y").parse(d.m4);
+        });
+
+        var metricName1 = cf.dimension(function (d) {
+            return d["m1"];
+        });
+        var metricNameGroup1 = metricName1.group().reduce(
+                function (p, v) {
+                    p += +v.value;
+                    return p;
+                },
+                function (p, v) {
+                    p -= +v.value;
+                    return p;
+                },
+                function () {
+                    return 0;
+                }
+        );
+        var metricName2 = cf.dimension(function (d) {
+            return d["m2"];
+        });
+        var metricNameGroup2 = metricName2.group().reduce(
+                function (p, v) {
+                    p += +v.value;
+                    return p;
+                },
+                function (p, v) {
+                    p -= +v.value;
+                    return p;
+                },
+                function () {
+                    return 0;
+                }
+        );
+        var metricName3 = cf.dimension(function (d) {
+            return d["m3"];
+        });
+        var metricNameGroup3 = metricName3.group().reduce(
+                function (p, v) {
+                    p += +v.value;
+                    return p;
+                },
+                function (p, v) {
+                    p -= +v.value;
+                    return p;
+                },
+                function () {
+                    return 0;
+                }
+        );
+        var metricName4 = cf.dimension(function (d) {
+            return +d.m4;
+        });
+        var metricNameGroup4 = metricName4.group().reduce(
+                function (p, v) {
+                    p += +v.value;
+                    return p;
+                },
+                function (p, v) {
+                    p -= +v.value;
+                    return p;
+                },
+                function () {
+                    return 0;
+                }
+        );
+        var metricName5 = cf.dimension(function (d) {
+            return d.m5;
+        });
+        var metricNameGroup5 = function (d) {
+            return d.type;
+//            var format = d3.format('02d');
+//            return d.m4.getFullYear() + '/' + format((d.m4.getMonth() + 1));
+        };
+
+        chart1
+                .dimension(metricName1)
+                .group(metricNameGroup1)
+                .controlsUseVisibility(true);
+        chart1.render();
+        chart2
+                .dimension(metricName2)
+                .group(metricNameGroup2)
+                .controlsUseVisibility(true);
+        chart2.render();
+        chart3
+                .dimension(metricName3)
+                .group(metricNameGroup3)
+                .controlsUseVisibility(true);
+        chart3.render();
+        var minDate = metricName4.bottom(1)[0].m4;
+        var maxDate = metricName4.top(1)[0].m4;
+        chart4
+                .height(75)
+                .brushOn(true)
+                .elasticY(true)
+                .dimension(metricName4)
+                .group(metricNameGroup4)
+                .showYAxis(false)
+                .centerBar(true)
+                .colors(['#303f9f'])
+                .mouseZoomable(true)
+                .barPadding(0.05)
+                .x(d3.time.scale().
+                        domain(
+                                [d3.time.month.offset(minDate, -1), d3.time.month.offset(maxDate, 1)]
+                                )
+                        )
+                .xUnits(d3.time.months)
+                .xAxis()
+                .ticks(d3.time.month, 1)
+                .tickFormat(d3.time.format("%b '%y"));
+
+        chart4.on("renderlet", function (chart) {
+            var gLabels = chart.select(".labels");
+            if (gLabels.empty()) {
+                gLabels = chart.select(".chart-body").append('g').classed('labels', true);
+            }
+
+            var gLabelsData = gLabels.selectAll("text").data(chart.selectAll(".bar")[0]);
+            gLabelsData.exit().remove(); //Remove unused elements
+
+            gLabelsData.enter().append("text"); //Add new elements
+
+            gLabelsData
+                    .attr('text-anchor', 'middle')
+                    .attr('fill', 'white')
+                    .text(function (d) {
+                        return d3.select(d).data()[0].data.value;
+                    })
+                    .attr('x', function (d) {
+                        return +d.getAttribute('x') + (d.getAttribute('width') / 2);
+                    })
+                    .attr('y', function (d) {
+                        return +d.getAttribute('y') + 15;
+                    })
+                    .attr('style', function (d) {
+                        if (+d.getAttribute('height') < 18)
+                            return "display:none";
+                    });
+        });
+        chart4.render();
+        chart5
+                .dimension(metricName5)
+                .group(metricNameGroup5)
+                .showGroups(false)
+                .size(100)
+                .columns([
+//                    Writing column name directly means the database column name is the one that will be displayed onscreen
+//                    'm6',
+                    {
+                        label: 'Name',
+                        format: function (d) {
+                            return d.m5;
+                        }
+                    },
+                    {
+                        label: 'Recommend to Hire',
+                        format: function (d) {
+                            return d.m6;
+                        }
+                    },
+                    {
+                        label: 'Recommended Role',
+                        format: function (d) {
+                            return d.m2;
+                        }
+                    },
+                    {
+                        label: 'Recommended Location',
+                        format: function (d) {
+                            return d.m1;
+                        }
+                    },
+                    {
+                        label: 'Recommended Salary',
+                        format: function (d) {
+                            return d.m7;
+                        }
+                    }
+                ]);
+        chart5.on("renderlet", function (chart) {
+
+//            CONDITIONAL FORMAT FOR YES/NO
+            $("#hiring_q5_chart5 td").each(function (index, Element) {
+                switch (Element.textContent) {
+                    case "YES":
+                        $(Element).css("color", "#4caf50");
+                        break;
+                    case "NO":
+                        $(Element).css("color", "#f44336");
+                        break;
+                }
+            });
+
+//            CONDITIONAL FORMAT TO CHECK IF NUMBER, RIGHT ALIGN, ELSE, LEFT ALIGN
+            var reg = new RegExp('^[0-9]+$');
+            $("#hiring_q5_chart5 td").each(function (index, Element) {
+                if (Element.textContent.match(reg) !== null) {
+                    $(Element).css("text-align", "right");
+                } else {
+                    $(Element).css("text-align", "left");
+                }
+            });
+            $("#hiring_q5_chart5 .dc-table-row").on("click", function () {
+                PopupCenter('/owen-prototype-jsp/popup.html', 'example', '900', '280');
+            });
+        });
+
+        chart5.render();
+
+        function PopupCenter(url, title, w, h) {
+            // Fixes dual-screen position                         Most browsers      Firefox
+            var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+            var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
+
+            var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+            var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+            var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+            var top = ((height / 2) - (h / 2)) + dualScreenTop;
+            var newWindow = window.open(url, title, 'scrollbars=yes, directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+            // Puts focus on the newWindow
+            if (window.focus) {
+                newWindow.focus();
+            }
+        }
+    });
 }
