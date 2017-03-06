e0025098842546644f6f12352504f4bab36182c3
rashmi <rashmi@192.168.1.14>
2017-03-06 10:01:10 +0530

    Attrition Tab

--- web/js/attrition.js
+++ web/js/attrition.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
     $('body').find('a[href$="tab1-panel"]').removeClass('vertical-mdl-tabs-disabled');
     $('body').find('a[href$="tab1-panel"]').addClass('is-active');
 
-    plotAttritionQ2Charts();
+//    plotAttritionQ2Charts();
     $('body').find('a[href$="tab2-panel"]').removeClass('vertical-mdl-tabs-disabled');
-
+//
     plotAttritionQ3Charts();
     $('body').find('a[href$="tab3-panel"]').removeClass('vertical-mdl-tabs-disabled');
 
@@ -27,12 +27,17 @@ function plotAttritionQ1Charts() {
     var chart2 = dc.selectMenu("#attrition_q1_chart2");
     var chart3 = dc.selectMenu("#attrition_q1_chart3");
     var chart4 = dc.barChart("#attrition_q1_chart4");
+    var chart5 = dc.pieChart("#attrition_q1_chart5");
+    var chart6 = dc.barChart("#attrition_q1_chart6");
+    var chart7 = dc.pieChart("#attrition_q1_chart7");
+    var chart8 = dc.numberDisplay("#attrition_q1_chart8");
 
     d3.csv("attrition_q1.csv", function (error, data) {
         var cf = crossfilter(data);
 
         data.forEach(function (d) {
             d.m4 = d3.time.format.utc("%d-%m-%y").parse(d.m4);
+            d.m7 = +d.m7;
         });
 
         var metricName1 = cf.dimension(function (d) {
@@ -99,22 +104,73 @@ function plotAttritionQ1Charts() {
                     return 0;
                 }
         );
+        var metricName5 = cf.dimension(function (d) {
+            return d["m5"];
+        });
+        var metricNameGroup5 = metricName5.group().reduce(
+                function (p, v) {
+                    p += +v.value;
+                    return p;
+                },
+                function (p, v) {
+                    p -= +v.value;
+                    return p;
+                },
+                function () {
+                    return 0;
+                }
+        );
+        var metricName6 = cf.dimension(function (d) {
+            return d["m6"];
+        });
+        var metricNameGroup6 = metricName6.group().reduce(
+                function (p, v) {
+                    p += +v.value;
+                    return p;
+                },
+                function (p, v) {
+                    p -= +v.value;
+                    return p;
+                },
+                function () {
+                    return 0;
+                }
+        );
+
+        var metricNameGroup7 = cf.groupAll().reduce(
+                function (p, v) {
+                    ++p.n;
+                    p.tot += v.m7;
+                    return p;
+                },
+                function (p, v) {
+                    --p.n;
+                    p.tot -= v.m7;
+                    return p;
+                },
+                function () {
+                    return {n: 0, tot: 0};
+                }
+        );
 
         chart1
                 .dimension(metricName1)
                 .group(metricNameGroup1)
                 .controlsUseVisibility(true);
         chart1.render();
+
         chart2
                 .dimension(metricName2)
                 .group(metricNameGroup2)
                 .controlsUseVisibility(true);
         chart2.render();
+
         chart3
                 .dimension(metricName3)
                 .group(metricNameGroup3)
                 .controlsUseVisibility(true);
         chart3.render();
+
         var minDate = metricName4.bottom(1)[0].m4;
         var maxDate = metricName4.top(1)[0].m4;
         chart4
@@ -133,10 +189,10 @@ function plotAttritionQ1Charts() {
                                 [d3.time.month.offset(minDate, -1), d3.time.month.offset(maxDate, 1)]
                                 )
                         )
-                .xUnits(d3.time.months)
-                .xAxis()
-                .ticks(d3.time.month, 1)
-                .tickFormat(d3.time.format("%b '%y"));
+                .xUnits(d3.time.months);
+//                .xAxis()
+//                .ticks(d3.time.month, 1)
+//                .tickFormat(d3.time.format("%b '%y"));
 
         chart4.on("renderlet", function (chart) {
             var gLabels = chart.select(".labels");
@@ -167,6 +223,88 @@ function plotAttritionQ1Charts() {
                     });
         });
         chart4.render();
+
+        chart5
+                .dimension(metricName3)
+                .group(metricNameGroup3)
+                .externalLabels(10)
+                .externalRadiusPadding(25)
+                .ordinalColors(['#ef5350', '#EC407A', '#AB47BC', '#7E57C2', '#5C6BC0', '#42A5F5', '#26C6DA', '#26A69A', '#66BB6A', '#9CCC65', '#D4E157', '#FFEE58', '#FFCA28', '#FFA726', '#FF7043'])
+                .label(function (d) {
+                    return d.key + ": " + d3.round((d.value / d3.sum(metricNameGroup3.all(), function (d) {
+                        return d.value;
+                    })) * 100, 1) + "%";
+                });
+        chart5.render();
+
+        chart6
+                .margins({top: 0, bottom: 30, left: 50, right: 20})
+                .dimension(metricName5)
+                .group(metricNameGroup5)
+//            .yAxisLabel("Count")
+                .elasticY(true)
+//            .showYAxis(false)
+                .x(d3.scale.ordinal().domain(metricName5)) // Need the empty val to offset the first value
+                .xUnits(dc.units.ordinal) // Tell Dc.js that we're using an ordinal x axis
+                .ordinalColors(['#7986CB'])
+//            .label(function (d) {
+//                return d.key + " = " + d.value;
+//            })
+                .centerBar(false);
+
+        chart6.on("renderlet", function (chart) {
+            var gLabels = chart.select(".labels");
+            if (gLabels.empty()) {
+                gLabels = chart.select(".chart-body").append('g').classed('labels', true);
+            }
+
+            var gLabelsData = gLabels.selectAll("text").data(chart.selectAll(".bar")[0]);
+            gLabelsData.exit().remove(); //Remove unused elements
+
+            gLabelsData.enter().append("text") //Add new elements
+
+            gLabelsData
+                    .attr('text-anchor', 'middle')
+                    .attr('fill', 'white')
+                    .text(function (d) {
+                        return d3.select(d).data()[0].data.value
+                    })
+                    .attr('x', function (d) {
+                        return +d.getAttribute('x') + (d.getAttribute('width') / 2);
+                    })
+                    .attr('y', function (d) {
+                        return +d.getAttribute('y') + 15;
+                    })
+                    .attr('style', function (d) {
+                        if (+d.getAttribute('height') < 18)
+                            return "display:none";
+                    });
+        });
+        chart6.render();
+
+        chart7
+                .dimension(metricName6)
+                .group(metricNameGroup6)
+                .externalLabels(10)
+                .externalRadiusPadding(25)
+                .ordinalColors(['#ef5350', '#EC407A', '#AB47BC', '#7E57C2', '#5C6BC0', '#42A5F5', '#26C6DA', '#26A69A', '#66BB6A', '#9CCC65', '#D4E157', '#FFEE58', '#FFCA28', '#FFA726', '#FF7043'])
+                .label(function (d) {
+                    return d.key + ": " + d3.round((d.value / d3.sum(metricNameGroup6.all(), function (d) {
+                        return d.value;
+                    })) * 100, 1) + "%";
+                });
+        chart7.render();
+
+        chart8
+                .formatNumber(d3.format(".3s"))
+                .valueAccessor(function (d) {
+                    return d.n ? d.tot / d.n : 0;
+                })
+                .group(metricNameGroup7);
+
+        setInterval(function () {
+            chart8.redraw();
+        }, 0);
     });
 }
 
@@ -324,88 +462,90 @@ function plotAttritionQ3Charts() {
     var chart2 = dc.selectMenu("#attrition_q3_chart2");
     var chart3 = dc.selectMenu("#attrition_q3_chart3");
     var chart4 = dc.barChart("#attrition_q3_chart4");
+    var chart5 = dc.barChart("#attrition_q3_chart5");
 
     d3.csv("attrition_q3.csv", function (error, data) {
         var cf = crossfilter(data);
 
         data.forEach(function (d) {
-            d.m4 = d3.time.format.utc("%d-%m-%y").parse(d.m4);
+            d.m4 = d3.time.format.utc("%d-%m-%Y").parse(d.m4);
         });
 
-        var tatMetricName1 = cf.dimension(function (d) {
+        var metricName1 = cf.dimension(function (d) {
             return d["m1"];
         });
-        var tatMetricNameGroup1 = tatMetricName1.group().reduce(
+        var metricNameGroup1 = metricName1.group().reduce(
                 function (p, v) {
-                    if (isCandidateCount(v)) {
-                        p += +v.value;
-                    }
+                    p += +v.value;
                     return p;
                 },
                 function (p, v) {
-                    if (isCandidateCount(v)) {
-                        p -= +v.value;
-                    }
+                    p -= +v.value;
                     return p;
                 },
                 function () {
                     return 0;
                 }
         );
-        var tatMetricName2 = cf.dimension(function (d) {
+        var metricName2 = cf.dimension(function (d) {
             return d["m2"];
         });
-        var tatMetricNameGroup2 = tatMetricName2.group().reduce(
+        var metricNameGroup2 = metricName2.group().reduce(
                 function (p, v) {
-                    if (isCandidateCount(v)) {
-                        p += +v.value;
-                    }
+                    p += +v.value;
                     return p;
                 },
                 function (p, v) {
-                    if (isCandidateCount(v)) {
-                        p -= +v.value;
-                    }
+                    p -= +v.value;
                     return p;
                 },
                 function () {
                     return 0;
                 }
         );
-        var tatMetricName3 = cf.dimension(function (d) {
+        var metricName3 = cf.dimension(function (d) {
             return d["m3"];
         });
-        var tatMetricNameGroup3 = tatMetricName3.group().reduce(
+        var metricNameGroup3 = metricName3.group().reduce(
                 function (p, v) {
-                    if (isCandidateCount(v)) {
-                        p += +v.value;
-                    }
+                    p += +v.value;
                     return p;
                 },
                 function (p, v) {
-                    if (isCandidateCount(v)) {
-                        p -= +v.value;
-                    }
+                    p -= +v.value;
                     return p;
                 },
                 function () {
                     return 0;
                 }
         );
-        var tatMetricName4 = cf.dimension(function (d) {
+        var metricName4 = cf.dimension(function (d) {
             return +d.m4;
         });
-        var tatMetricNameGroup4 = tatMetricName4.group().reduce(
+        var metricNameGroup4 = metricName4.group().reduce(
                 function (p, v) {
-                    if (isCandidateCount(v)) {
-                        p += +v.value;
-                    }
+                    p += +v.value;
                     return p;
                 },
                 function (p, v) {
-                    if (isCandidateCount(v)) {
-                        p -= +v.value;
-                    }
+                    p -= +v.value;
+                    return p;
+                },
+                function () {
+                    return 0;
+                }
+        );
+
+        var metricName5 = cf.dimension(function (d) {
+            return d.m5;
+        });
+        var metricNameGroup5 = metricName5.group().reduce(
+                function (p, v) {
+                    p += +v.value;
+                    return p;
+                },
+                function (p, v) {
+                    p -= +v.value;
                     return p;
                 },
                 function () {
@@ -414,28 +554,28 @@ function plotAttritionQ3Charts() {
         );
 
         chart1
-                .dimension(tatMetricName1)
-                .group(tatMetricNameGroup1)
+                .dimension(metricName1)
+                .group(metricNameGroup1)
                 .controlsUseVisibility(true);
         chart1.render();
         chart2
-                .dimension(tatMetricName2)
-                .group(tatMetricNameGroup2)
+                .dimension(metricName2)
+                .group(metricNameGroup2)
                 .controlsUseVisibility(true);
         chart2.render();
         chart3
-                .dimension(tatMetricName3)
-                .group(tatMetricNameGroup3)
+                .dimension(metricName3)
+                .group(metricNameGroup3)
                 .controlsUseVisibility(true);
         chart3.render();
-        var minDate = tatMetricName4.bottom(1)[0].m4;
-        var maxDate = tatMetricName4.top(1)[0].m4;
+        var minDate = metricName4.bottom(1)[0].m4;
+        var maxDate = metricName4.top(1)[0].m4;
         chart4
                 .height(75)
                 .brushOn(true)
                 .elasticY(true)
-                .dimension(tatMetricName4)
-                .group(tatMetricNameGroup4)
+                .dimension(metricName4)
+                .group(metricNameGroup4)
                 .showYAxis(false)
                 .centerBar(true)
                 .colors(['#303f9f'])
@@ -446,10 +586,10 @@ function plotAttritionQ3Charts() {
                                 [d3.time.month.offset(minDate, -1), d3.time.month.offset(maxDate, 1)]
                                 )
                         )
-                .xUnits(d3.time.months)
-                .xAxis()
-                .ticks(d3.time.month, 1)
-                .tickFormat(d3.time.format("%b '%y"));
+                .xUnits(d3.time.months);
+//                .xAxis()
+//                .ticks(d3.time.month, 1)
+//                .tickFormat(d3.time.format("%b '%y"));
 
         chart4.on("renderlet", function (chart) {
             var gLabels = chart.select(".labels");
